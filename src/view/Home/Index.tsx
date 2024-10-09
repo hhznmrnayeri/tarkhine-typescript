06943c7877fc6e84cb0b7c94711824cfb52faca6
Heydar <h_h_z_n@yahoo.com>
2024-10-09 12:43:57 +0330

    build: :lipstick: create SearchBox component
    create SearchBox component with handleSearch logic
    
    import to home page

--- src/view/Home/Index.tsx
+++ src/view/Home/Index.tsx
@@ -1,10 +1,12 @@
 import HeaderSlider from "../../components/HeaderSlider";
 import Nav from "../../components/Nav";
+import SearchBox from "../../components/SearchBox";
 export default function Index() {
   return (
     <>
       <Nav title="home" />
       <HeaderSlider title="تجربه غذای سالم گیاهی به سبک ترخینه" />
+      <SearchBox />
     </>
   );
 }
