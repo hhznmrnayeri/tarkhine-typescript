704a11854d20b4c06bf19706f767e5438f18d039
Heydar <h_h_z_n@yahoo.com>
2024-10-09 15:03:25 +0330

    build: :lipstick: create Topic section in home page
    create Topic section with types
    
    create BaseUrl and start database

---
+++ src/types/Branch.types.ts
@@ -0,0 +1,9 @@
+export type BranchType = {
+  id: string;
+  title: string;
+  img: string;
+  address: string;
+  phone: string;
+  start_time: string;
+  finish_time: string;
+};
--- src/view/Home/Index.tsx
+++ src/view/Home/Index.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
+import { BaseUrl } from "../../components/BaseUrl";
 import HeaderSlider from "../../components/HeaderSlider";
 import Nav from "../../components/Nav";
 import SearchBox from "../../components/SearchBox";
+import Topic from "./Topic";
+import { TopicType } from "../../types/Topic.types";
+import { BranchType } from "../../types/Branch.types";
 export default function Index() {
+  const [topicArray, setTopicArray] = useState<TopicType[]>([]);
+  const [branchArray, setBranchArray] = useState<BranchType[]>([]);
+  function getTopic() {
+    fetch(`${BaseUrl}/topics`)
+      .then((res) => res.json())
+      .then((data) => setTopicArray(data));
+  }
+  function getBranch() {
+    fetch(`${BaseUrl}/branches`)
+      .then((res) => res.json())
+      .then((data) => setBranchArray(data));
+  }
+  useEffect(() => {
+    getTopic();
+    getBranch();
+  }, []);
   return (
     <>
       <Nav title="home" />
       <HeaderSlider title="تجربه غذای سالم گیاهی به سبک ترخینه" />
       <SearchBox />
+      <Topic topicArray={topicArray} branchArray={branchArray} />
     </>
   );
 }
