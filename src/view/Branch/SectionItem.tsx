73048c0b16f08803014264dcc0d4ef793742c27c
Heydar <h_h_z_n@yahoo.com>
2024-10-11 00:45:02 +0330

    build: :lipstick: create SectionItem component
    create SectionItem component and control lists
    
    import to branch page

---
+++ src/view/Branch/SectionItem.tsx
@@ -0,0 +1,79 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import SectionTitle from "./SectionTitle";
+import SlideItem from "./SlideItem";
+import ArrowLeftIcon from "../../assets/svg/ArrowLeftIcon";
+import ArrowRightIcon from "../../assets/svg/ArrowRightIcon";
+import { FoodTypes } from "../../types/Food.types";
+type SectionItemProps = {
+  array: FoodTypes[];
+  label: string;
+};
+export default function SectionItem({ array, label }: SectionItemProps) {
+  return (
+    <section
+      className={`mt-6  mb-6  ${label === "popular" ? "bg-primary" : ""} py-6`}
+    >
+      <div className="container">
+        {label === "special" && <SectionTitle title="پیشنهاد ویژه" />}
+        {label === "popular" && (
+          <SectionTitle title="غذاهای محبوب" color="text-white" />
+        )}
+        {label === "foreign" && <SectionTitle title="غذاهای غیر ایرانی" />}
+        {/* swiper */}
+        <div className="mt-3 md:mt-6 relative">
+          <Swiper
+            slidesPerView={1.2}
+            spaceBetween={16}
+            navigation={{
+              nextEl: ".next__btn--swiper1",
+              prevEl: ".prev__btn--swiper1",
+            }}
+            breakpoints={{
+              350: {
+                slidesPerView: 1.4,
+                spaceBetween: 16,
+              },
+              480: {
+                slidesPerView: 1.9,
+                spaceBetween: 16,
+              },
+              640: {
+                slidesPerView: 2.5,
+                spaceBetween: 16,
+              },
+              768: {
+                slidesPerView: 2.5,
+                spaceBetween: 24,
+              },
+              1024: {
+                slidesPerView: 3.5,
+                spaceBetween: 24,
+              },
+              1280: {
+                slidesPerView: 4.3,
+                spaceBetween: 24,
+              },
+            }}
+            modules={[Navigation]}
+            className=""
+          >
+            {array.map((item) => (
+              <SwiperSlide key={`${label}-${item.id}`}>
+                <SlideItem {...item} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <button className="next__btn--swiper1 absolute top-64 -left-5 bg-white w-10 h-10 m-auto z-10 hidden lg:flex p-1 rounded-lg border border-gray-400 text-gray-600">
+            <ArrowLeftIcon size="w-8 h-8" />
+          </button>
+          <button className="prev__btn--swiper1 absolute top-64 -right-5 bg-white w-10 h-10 m-auto z-10 hidden lg:flex p-1 rounded-lg border border-gray-400 text-gray-600">
+            <ArrowRightIcon size="w-8 h-8" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
