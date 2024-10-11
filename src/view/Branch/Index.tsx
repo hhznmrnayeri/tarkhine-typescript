d7b23261cde7084dfb0900fa53d5fe453a2d6f96
Heydar <h_h_z_n@yahoo.com>
2024-10-12 00:01:05 +0330

    build: :lipstick: create menu btn
    create menu btn with icon
    
    use to branch page

--- src/view/Branch/Index.tsx
+++ src/view/Branch/Index.tsx
@@ -5,6 +5,8 @@ import SearchBox from "../../components/SearchBox";
 import { BaseUrl } from "../../components/BaseUrl";
 import { FoodTypes } from "../../types/Food.types";
 import SectionItem from "./SectionItem";
+import { NavLink } from "react-router-dom";
+import NoteIcon from "../../assets/svg/NoteIcon";
 export function Index() {
   const [specialArray, setSpecialArray] = useState<FoodTypes[]>([]);
   const [popularArray, setPopularArray] = useState<FoodTypes[]>([]);
@@ -40,6 +42,13 @@ export function Index() {
       <SectionItem array={specialArray} label="special" />
       <SectionItem array={popularArray} label="popular" />
       <SectionItem array={foreignArray} label="foreign" />
+      <NavLink
+        to="/menu"
+        className="flex-center mx-auto border mt-3 md:mt-7 border-primary text-primary gap-2 p-2 rounded md:px-4 md:font-estedadMedium text-xs md:text-base w-52"
+      >
+        <NoteIcon size="w-4 h-4 md:w-6 md:h-6" />
+        مشاهده منوی کامل
+      </NavLink>
     </>
   );
 }
