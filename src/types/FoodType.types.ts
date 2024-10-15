58d192ac1edb2b9e201bbc78d3e7517b2500b6e9
Heydar <h_h_z_n@yahoo.com>
2024-10-16 00:02:45 +0330

    build: :lipstick: create Type section
    create Type section with type
    
    import to Menu page

---
+++ src/types/FoodType.types.ts
@@ -0,0 +1,6 @@
+export type FoodType = {
+  id: string;
+  title?: string;
+  value?: string;
+  btn?: boolean;
+};
--- src/view/Menu/Index.tsx
+++ src/view/Menu/Index.tsx
@@ -2,24 +2,40 @@ import { useEffect, useState } from "react";
 import HeaderSlider from "../../components/HeaderSlider";
 import Nav from "../../components/Nav";
 import Topic from "./Topic";
+import Type from "./Type";
 import { TopicType } from "../../types/Topic.types";
+import { FoodType } from "../../types/FoodType.types";
 import { BaseUrl } from "../../components/BaseUrl";
 import Footer from "../../components/Footer";
 export function Index() {
   const [topicArray, setTopicArray] = useState<TopicType[]>([]);
+  const [typeArray, setTypeArray] = useState<FoodType[]>([]);
   function getTopics() {
     fetch(`${BaseUrl}/topics`)
       .then((res) => res.json())
       .then((data) => setTopicArray(data));
   }
+  function getType() {
+    fetch(`${BaseUrl}/types`)
+      .then((res) => res.json())
+      .then((data) => setTypeArray(data));
+  }
   useEffect(() => {
     getTopics();
+    getType();
   }, []);
   return (
     <>
       <Nav title="menu" />
       <HeaderSlider title="لذت غذای سالم و گیاهی را با ترخینه تجربه کنید!" />
       <Topic topicArray={topicArray} />
+      <section className="mt-2 md:mt-4">
+        <div className="container">
+          <div className="flex flex-col md:flex-row items-start md:items-center gap-3 justify-between">
+            <Type typeArray={typeArray} />
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
