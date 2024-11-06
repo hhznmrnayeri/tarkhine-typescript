42e38bd7f3bb50e3be7a77a25f724e59df5fab9e
Heydar <h_h_z_n@yahoo.com>
2024-11-06 13:21:45 +0330

    build: :lipstick: create OrderItem component
    create OrderItem component with type
    
    create OrderItem component with icon

---
+++ src/types/OrderItem.types.ts
@@ -0,0 +1,19 @@
+export type OrderItemType = {
+  id: string;
+  topicId?: string;
+  typeId?: string;
+  isFavorite?: boolean;
+  isSpecial?: boolean;
+  isPopular?: boolean;
+  isForeign?: boolean;
+  img: string[];
+  title: string;
+  resepi?: string;
+  offerWrapper?: string;
+  offerPrice?: string;
+  offerCount?: string;
+  priceValue?: string;
+  comment?: string;
+  star: number;
+  count: number;
+};
