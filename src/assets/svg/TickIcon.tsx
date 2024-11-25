7c9bbef9cc934c6c670e30471e9104601c3df00b
Heydar <h_h_z_n@yahoo.com>
2024-11-25 10:01:31 +0330

    build: :lipstick: create WalletSection in profile page
    create WalletSection with type
    
    create WalletSection in profile page

---
+++ src/assets/svg/TickIcon.tsx
@@ -0,0 +1,11 @@
+import { Icon } from "../../types/Icons.types";
+export default function TickIcon({ size }: Icon): React.ReactNode {
+  return (
+    <svg className={`${size}`} fill="none" viewBox="0 0 10 8">
+      <path
+        fill="#417F56"
+        d="M3.581 7.58a.75.75 0 01-.53-.22L.221 4.53a.754.754 0 010-1.06c.29-.29.77-.29 1.06 0l2.3 2.3L8.721.63c.29-.29.77-.29 1.06 0 .29.29.29.77 0 1.06l-5.67 5.67a.75.75 0 01-.53.22z"
+      ></path>
+    </svg>
+  );
+}
